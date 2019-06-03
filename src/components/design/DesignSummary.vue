493143907e26d9c7875937ad1305a34380d6e73a
Gravitas Hunt <onagravitashunt@gmail.com>
2019-06-03 21:56:04 +0100

    save and exit button added to summary in preparation for removing header

--- src/components/design/DesignSummary.vue
+++ src/components/design/DesignSummary.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="design-summary" :style="[gridStyle]">
-    <div class="right-align col1">Class Name</div>
+    <div class="center-align back-button" @click="saveAndExit">
+      &lang;
+    </div>
     <div class="left-align b-right">
       <input type="text" class="class-name-input" v-model="className">
     </div>
     <div class="right-align">Platform Type</div>
-    <div class="left-align double-right">{{platformType}}</div>
+    <div class="right-align double-right">{{platformType}}</div>
     <div class="center-align span2">{{finalStats.ev}}% Ev</div>
     <div class="center-align span2 b-right">{{finalStats.pen}}% Pen</div>
     <div class="center-align double-right build-time">{{buildTime}} Years</div>
     <!--  -->
-    <div class="right-align col1">Capabilities</div>
-    <div class="left-align b-right capabilities">{{capabilities}}</div>
+    <div class="left-align b-top b-right capabilities col2">{{capabilities}}</div>
     <div class="right-align b-top">Design Weight</div>
     <div class="right-align b-top double-right">{{weights.design}}kt</div>
     <div class="center-align b-top span2">{{finalStats.br}}BR</div>
@@ -20,8 +21,7 @@
     <div class="center-align b-right b-top crew-stat">EN{{finalStats.en}}</div>
     <div class="center-align double-right b-top crew-stat">T{{finalStats.t}}</div>
     <!--  -->
-    <div class="col1"></div>
-    <div class="right-align">Max Weight</div>
+    <div class="right-align max-weight col3">Max Weight</div>
     <div class="right-align double-right">{{weights.max}}kt</div>
     <div v-for="s of shipStats" :key="s" class="b-right b-top center-align rounded-stat">
       {{s.toUpperCase()}}{{finalStats[s]}}
@@ -95,8 +95,12 @@ export default {
     },
     gridStyle () {
       return {
-        gridTemplateColumns: `120px ${this.col2Width - 159}px 110px 90px repeat(7,40px) 100px 70px 90px`
+        gridTemplateColumns: `70px ${this.col2Width - 109}px 110px 90px repeat(7,40px) 100px 70px 90px`
       }
+    },
+    // saving
+    editingDesign () {
+      return this.$store.state.editingDesign
     }
   },
   methods: {
@@ -109,6 +113,31 @@ export default {
     },
     roundPercents (s) {
       return Math.round(s * 1000) / 10
+    },
+    saveDesign () {
+      if (this.$store.state.design.className) {
+        let design = JSON.parse(JSON.stringify(this.$store.state.design))
+        let savedDesign = {
+          name: design.className,
+          stats: this.$store.getters.roundedStats,
+          design
+        }
+        if (this.editingDesign === null) {
+          this.$store.dispatch('saveNewDesign', savedDesign)
+        } else {
+          this.$store.dispatch('saveExistingDesign', savedDesign)
+        }
+        return true
+      }
+      alert('Please enter a name for the class!')
+      return false
+    },
+    saveAndExit () {
+      if (this.saveDesign()) {
+        this.$router.push('/')
+        this.$store.dispatch('clearDesign')
+        this.$store.commit('clearEditing')
+      }
     }
   }
 }
@@ -130,17 +159,30 @@ export default {
 .col1 {
   grid-column-start: 1;
 }
+.col2 {
+  grid-column-start: 2;
+}
+.col3 {
+  grid-column-start: 3;
+}
 .build-time {
   grid-column: span 3;
 }
 .capabilities {
   grid-row: span 2;
   font-size: 10pt;
-  line-height: 20px;
+  line-height: 18px;
+  padding-top: 2px;
   overflow-y: auto;
 }
 .span2 {
   grid-column: span 2;
+}
+.back-button {
+  grid-row: 1 / 4;
+  line-height: 90px;
+  font-size: 50pt;
+  border-right: 2px solid black;
 }
 .left-align {
   text-align: left;
