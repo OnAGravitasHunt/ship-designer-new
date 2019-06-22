fe07395a5580c8aabf9ac6aa19c83f1b11823e69
Gravitas Hunt <onagravitashunt@gmail.com>
2019-06-22 18:11:09 +0100

    deploy

---
+++ precache-manifest.1f05ec145e3cca860917c6d1cf4e4584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a38a23a5e70e968c28e",
+    "url": "/ship-designer-new/css/app.4d4f76ac.css"
+  },
+  {
+    "revision": "4a38a23a5e70e968c28e",
+    "url": "/ship-designer-new/js/app.cafb3927.js"
+  },
+  {
+    "revision": "6df30d88c3609857b9dc",
+    "url": "/ship-designer-new/css/chunk-vendors.a031dfb3.css"
+  },
+  {
+    "revision": "6df30d88c3609857b9dc",
+    "url": "/ship-designer-new/js/chunk-vendors.a71d1195.js"
+  },
+  {
+    "revision": "aeb99e40b32d50df6b8f43f1fdc94ac2",
+    "url": "/ship-designer-new/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ship-designer-new/robots.txt"
+  }
+];
